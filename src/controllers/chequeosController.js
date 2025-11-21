959f9e342f8a9fd03b94bb72b60b33fb0a40b066
Lucas Martin Schlez <lucasschlez@gmail.com>
2025-11-21 00:16:29 -0300

    a

--- src/controllers/chequeosController.js
+++ src/controllers/chequeosController.js
@@ -2,11 +2,13 @@ import { Chequeo } from '../models/Chequeo.js';
 
 export async function createCheck(req, res, next) {
   try {
-    const { owner, fecha, tipo, variable1,variable2,variable3,variable4,variable5,variable6,variable7 } = req.body;
-
+    const { userId, fecha, tipo, variable1,variable2,variable3,variable4,variable5,variable6,variable7 } = req.body;
+    if (!userId) {
+      return res.status(400).json({ message: 'userId es obligatorio' });
+    }
     const check = await Check.create({
         
-      owner, 
+      owner: userId, 
       fecha, 
       tipo,
       variable1,
