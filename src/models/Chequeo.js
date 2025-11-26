440c0db246e803a25268d691fd706b51e608fa15
Lucas Martin Schlez <79287228+lucotuco@users.noreply.github.com>
2025-11-26 15:27:32 -0300

    Fix chequeo creation handling

--- src/models/Chequeo.js
+++ src/models/Chequeo.js
@@ -11,40 +11,38 @@ const chequeoSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
-    tipo:
-    {
+    tipo: {
       type: String,
       required: true,
     },
-    variable1:{
-        type: Number,
-        required: false,
+    variable1: {
+      type: Number,
+      required: false,
     },
-    variable2:{
-        type: Number,
-        required: false,
+    variable2: {
+      type: Number,
+      required: false,
     },
-
-    variable3:{
-        type: Number,
-        required: false,
+    variable3: {
+      type: Number,
+      required: false,
     },
-    variable4:{
-        type: Number,
-        required: false,
+    variable4: {
+      type: Number,
+      required: false,
     },
-    variable5:{
-        type: Number,
-        required: false,
+    variable5: {
+      type: Number,
+      required: false,
     },
-    variable6:{
-        type: Number,
-        required: false,
+    variable6: {
+      type: Number,
+      required: false,
     },
-    variable7:{
-        type: Number,
-        required: false,
-    }
+    variable7: {
+      type: Number,
+      required: false,
+    },
   },
   { timestamps: true }
 );
