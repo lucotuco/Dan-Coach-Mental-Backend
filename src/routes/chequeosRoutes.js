ca0288b94cbe5d7b8e35cfb5f43bb5a8354f61ca
Lucas Martin Schlez <lucasschlez@gmail.com>
2025-12-01 12:08:50 -0300

    jjojo

--- src/routes/chequeosRoutes.js
+++ src/routes/chequeosRoutes.js
@@ -2,9 +2,8 @@ import { Router } from 'express';
 import multer from 'multer';
 import { createCheck, listChecksByTypeAndOwner } from '../controllers/chequeosController.js';
 
-const upload = multer({ dest: 'uploads/' });
-
 const router = Router();
+const upload = multer({ dest: 'uploads/' });
 
 router.get('/', listChecksByTypeAndOwner);
 router.post('/', upload.single('audio'), createCheck);
