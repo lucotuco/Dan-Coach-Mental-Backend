5fe51fd44ed01276a93c9a985681f56ea96f47c8
Lucas Martin Schlez <lucasschlez@gmail.com>
2025-12-02 18:18:01 -0300

    test12

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -118,7 +118,7 @@ export async function loginUser(req, res, next) {
         message: 'competitionType debe ser individual, pareja o equipo',
       });
     }
-
+    console.log(`user del front: ${id}`);
     let parsedBirthDate;
     if (birthDate) {
       parsedBirthDate = new Date(birthDate);
@@ -133,12 +133,12 @@ export async function loginUser(req, res, next) {
     if (competitionType !== undefined) updateData.competitionType = competitionType;
     if (level !== undefined) updateData.level = level;
 
-    console.log(updateData);
+    console.log(`data a actualizar: ${updateData}`);
     const updatedUser = await User.findByIdAndUpdate(id, updateData, {
       new: true,
       //runValidators: true,
     });
-    console.log(updatedUser);
+    console.log(`user a actualizar: ${updatedUser}`);
     if (!updatedUser) {
       return res.status(404).json({ message: 'Usuario no encontrado' });
     }
