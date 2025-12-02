52490d0bd42aef5859b54ddfb535d3b7269f9f32
Lucas Martin Schlez <lucasschlez@gmail.com>
2025-12-02 16:31:50 -0300

    test4

--- src/models/User.js
+++ src/models/User.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    phone:{
+      type: Number,
+      require: false,
+    },
     birthDate: {
       type: Date,
       required: false,
