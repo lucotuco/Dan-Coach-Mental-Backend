fb7f398cc49b93576114c86e4bcc12084b9c8600
Lucas Martin Schlez <lucasschlez@gmail.com>
2025-12-05 17:09:09 -0300

    test24

--- src/controllers/realtimeController.js
+++ src/controllers/realtimeController.js
@@ -94,7 +94,7 @@ Usá esta info SOLO como contexto. Volvé a preguntarle cómo se siente hoy para
           instructions,
           audio: {
               output: {
-                voice: "cedar", 
+                voice: "ash", 
                       }
                  }
           // Opcional: si querés texto + audio
