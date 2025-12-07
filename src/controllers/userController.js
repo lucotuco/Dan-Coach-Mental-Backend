86d29958ef676c42290d3f0a6b306f4d71db349e
Lucas Martin Schlez <lucasschlez@gmail.com>
2025-12-07 17:59:27 -0300

    test40

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -220,7 +220,7 @@ export async function updateUserGoalA(req, res, next) {
 
     const transcript = await transcribeAudio(req.file);
     const { summary, tags } = await getSummaryAndTagsFromTranscript(transcript);
-
+    console.log('summary: ', summary, 'tags: ' ,tags);
     audioData = {
       url: null,
       transcript,
@@ -239,7 +239,7 @@ export async function updateUserGoalA(req, res, next) {
 
     const updatedUser = await User.findByIdAndUpdate(
       id,
-      { goalAudio: audioData },
+      { goalA: audioData },
       { new: true }
     );
 
