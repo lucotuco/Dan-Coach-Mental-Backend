a3e480110c6913b99916b6efe3c962b3674775d5
Lucas Martin Schlez <lucasschlez@gmail.com>
2026-02-26 17:03:24 -0300

    fix9

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from'bcryptjs';
 import jwt from'jsonwebtoken';
-import User from'../models/User';
+import User from'../models/User.js';
 
 const normalizeEmail = (email) => String(email || '').trim().toLowerCase();
 
