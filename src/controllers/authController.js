c6b51203dc4304482797353f7765c455d37a1b82
Lucas Martin Schlez <lucasschlez@gmail.com>
2026-02-26 16:37:54 -0300

    fix7

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+import bcrypt from'bcryptjs';
+import jwt from'jsonwebtoken';
+import User from'../models/User';
 
 const normalizeEmail = (email) => String(email || '').trim().toLowerCase();
 
