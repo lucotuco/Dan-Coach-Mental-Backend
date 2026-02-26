7e3ef523a72720ce1f19de6e8144afc279940838
Lucas Martin Schlez <lucasschlez@gmail.com>
2026-02-26 16:20:50 -0300

    fix 3

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
-const authController = require('../controllers/authController');
+import authController from '../controllers/authController';
 
 const router = Router();
 
