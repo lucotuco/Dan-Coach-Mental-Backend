8cc2e69fc128cd2669a84cf1ba8594a015750d26
Lucas Martin Schlez <lucasschlez@gmail.com>
2026-02-26 16:27:43 -0300

    fix5

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
-import authController from '../controllers/authController';
+import {register,login} from '../controllers/authController';
 
 const router = Router();
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
