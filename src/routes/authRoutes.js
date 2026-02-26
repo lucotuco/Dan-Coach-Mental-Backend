8e78be754ba1563c859d24260f472823c85b79b8
Lucas Martin Schlez <lucasschlez@gmail.com>
2026-02-26 16:33:56 -0300

    fix6

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
-import {register,login} from '../controllers/authController';
+import {register,login} from '../controllers/authController.js';
 
 const router = Router();
 
