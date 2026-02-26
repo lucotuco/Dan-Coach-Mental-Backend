f3e70adbe9aaf71bccfa9962497380346d631a77
Lucas Martin Schlez <lucasschlez@gmail.com>
2026-02-26 19:01:40 -0300

    fix 2

--- src/middleware/requireTeam.js
+++ src/middleware/requireTeam.js
@@ -1,9 +1,8 @@
-// src/middleware/requireTeam.js
-module.exports = function requireTeam() {
+export default function requireTeam() {
   return (req, res, next) => {
     if (!req.user?.teamId) {
       return res.status(400).json({ error: 'User has no team' });
     }
     next();
   };
-};
+}
